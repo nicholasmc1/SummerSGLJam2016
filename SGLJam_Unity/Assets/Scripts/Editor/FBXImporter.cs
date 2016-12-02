ccb50202e5f9bce95f66caccc594b97da66097f3
Nicholas McDonnell <nicholasmcdonnell1@gmail.com>
2016-12-03 09:17:34 +1100

    Added in control. Added simple pipeline extensions

---
+++ SGLJam_Unity/Assets/Scripts/Editor/FBXImporter.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class FBXImporter : AssetPostprocessor
+{
+    void OnPreprocessModel()
+    {
+        if (assetImporter.userData != "Asset Post Proccesed")
+        {
+            ModelImporter importer = assetImporter as ModelImporter;
+            string path = importer.assetPath.ToLower();
+            if (!path.Contains("extensions"))//applies to all meshes I  made.
+            {
+                importer.materialName = ModelImporterMaterialName.BasedOnMaterialName;
+                importer.materialSearch = ModelImporterMaterialSearch.Everywhere;
+                importer.importBlendShapes = false;
+                if (path.Contains("environment") || path.Contains("props"))
+                {
+                    importer.animationType = ModelImporterAnimationType.None;
+                    importer.importAnimation = false;
+                }
+            }
+            
+            
+            assetImporter.userData = "Asset Post Proccesed";
+        }
+    }
+}
---
+++ SGLJam_Unity/Assets/Scripts/Editor/TexImporter.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+public class TexImporter : AssetPostprocessor
+{
+    void OnPreprocessTexture()
+    {
+
+        if (assetImporter.userData != "Asset Post Proccesed")
+        {
+            TextureImporter importer = assetImporter as TextureImporter;
+            string path = importer.assetPath.ToLower();
+            if (!path.Contains("extentions"))
+            {
+                if (path.Contains("assets/textures"))
+                {
+                    if (path.Contains("_nm"))
+                    {
+                        importer.normalmap = true;
+                        importer.textureType = TextureImporterType.NormalMap;
+                    }
+                }
+
+                if (path.Contains("assets/ui/textures"))
+                    importer.textureType = TextureImporterType.Sprite;
+
+
+                assetImporter.userData = "Asset Post Proccesed";
+            }
+        }
+    }
+}
