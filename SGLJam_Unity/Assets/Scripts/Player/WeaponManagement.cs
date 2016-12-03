f1f46bcaf67b0c520630de613917d5a56b470a9c
Chris Kelly <1654454@student.swin.edu.au>
2016-12-04 10:42:03 +1100

    Ball appears on weapon when charging
    - Ball is attached to the end of the gun while charging before firing
    - Currently has a particle issue, will fix.

--- SGLJam_Unity/Assets/Scripts/Player/WeaponManagement.cs
+++ SGLJam_Unity/Assets/Scripts/Player/WeaponManagement.cs
@@ -8,14 +8,15 @@ public class WeaponManagement : StateBehaviour {
 	public Transform shootOrigin;
 	public bool charging;
 	public float maxSpeed;
-	private float speed;
+	public float speed;
+	public float chargeSpeed;
 	private Rigidbody playerMove;
+	private float _timeElapsed;
 
     private GameObject _blinkBall;
 
 	void Awake () {
 		StartCoroutine (SetPlayer ());
-		speed = 5;
 	}
 
 	IEnumerator SetPlayer () {
@@ -23,26 +24,45 @@ public class WeaponManagement : StateBehaviour {
 		playerMove = PlayerCore._instance.gameObject.GetComponent<Rigidbody> ();
 	}
 
+	public void PrepareToFire() {
+		if (_blinkBall != null) {
+			Destroy(_blinkBall);
+		}
+		charging = true;
+		speed = 20;
+		_blinkBall = Instantiate (projectilePrefab, shootOrigin.position, shootOrigin.rotation) as GameObject;
+		_blinkBall.transform.parent = shootOrigin;
+		_blinkBall.GetComponent<Rigidbody> ().isKinematic = true;
+	}
+
 	public override void UpdatePlaying() {
-		if (charging && speed < maxSpeed) {
-			speed += Time.deltaTime * maxSpeed;
-			Debug.Log (speed);
+		if (charging && _blinkBall != null) {
+			if (speed < maxSpeed) {
+				speed += Time.deltaTime * chargeSpeed * maxSpeed;
+				_timeElapsed += Time.deltaTime;
+				Debug.Log ("Speed: " + speed + "  Time: " + _timeElapsed);
+			}
+
 		}
 	}
 
+	public override void FixedUpdatePlaying() {
+//		if (charging && _blinkBall != null) {
+//			_blinkBall.transform.position = shootOrigin.position;
+//		}
+	}
+
 	public BlinkBall Fire() {
-		if (_blinkBall != null) {
-            Destroy(_blinkBall);
-		}
 		//Debug.Log ("Pew!");
 		charging = false;
-		_blinkBall = Instantiate (projectilePrefab, shootOrigin.position, shootOrigin.rotation) as GameObject;
 		Vector3 v = transform.parent.forward * speed;
 		v += playerMove.velocity;
 		v = Vector3.ClampMagnitude (v, 75);
 		//Debug.Log (v);
+		_blinkBall.GetComponent<Rigidbody> ().isKinematic = false;
+		_blinkBall.transform.parent = null;
 		_blinkBall.GetComponent<Rigidbody> ().velocity = v;
-		speed = 5;
+		_timeElapsed = 0;
         return _blinkBall.GetComponent<BlinkBall>();
 	}
 }
