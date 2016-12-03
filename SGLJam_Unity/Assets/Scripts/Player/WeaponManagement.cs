3a37c57a7cb1af7a2f4b4690a96be0c35d95828d
Chris Kelly <1654454@student.swin.edu.au>
2016-12-03 12:30:16 +1100

    Added the bazooka
    - Added a capsule to the player to be the bazooka
    - Fires on LMB
    - Added two new layers and changed some collisions
    - Added player prefab
    - Added blinkball prefab

---
+++ SGLJam_Unity/Assets/Scripts/Player/WeaponManagement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponManagement : StateBehaviour {
+
+	public GameObject projectilePrefab;
+	public Transform shootOrigin;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void Fire() {
+		Debug.Log ("Pew!");
+		GameObject projectile = Instantiate (projectilePrefab, shootOrigin.position, shootOrigin.rotation) as GameObject;
+		projectile.GetComponent<Rigidbody> ().velocity = transform.TransformDirection (new Vector3 (0, speed, 0));
+	}
+}
