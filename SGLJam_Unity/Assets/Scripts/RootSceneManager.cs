9928b18d7c8ac44c8d8cbb880c07a61a24993f9b
Semirix <me@semirix.net>
2016-12-03 14:18:12 +1100

    Added RootSceneManager

---
+++ SGLJam_Unity/Assets/Scripts/RootSceneManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class RootSceneManager : StateBehaviour {
+	private static RootSceneManager _instance;
+
+	private string[] _levels;
+	private int _currentLevel;
+	private const string _transitionRoom = "Scenes/TransitionRoom";
+
+	private RootSceneManager() {}
+
+	public static RootSceneManager GetInstance() {
+		if (_instance != null) {
+
+		} else {
+			_instance = RootSceneManager ();
+		}
+	}
+
+	private IEnumerator RemoveLevel(string level, System.Action<int> callback) {
+		yield return SceneManager.UnloadSceneAsync (level);
+		callback (0);
+	}
+
+	private IEnumerator LoadLevel(string level, System.Action<int> callback) {
+		yield return SceneManager.LoadSceneAsync (level, LoadSceneMode.Additive);
+		callback (0);
+	}
+
+	private IEnumerator SetupNextLevel(string nextLevel, string previousLevel) {
+		// TODO: Close doors
+		StartCoroutine(LoadLevel(nextLevel, levelLoaded => {
+			StartCoroutine(RemoveLevel(previousLevel, levelRemoved => {
+				// TODO: Open doors
+			}));
+		}));
+	}
+}
